3c5956442dba00883f442d128ceea786c75582ba
gemperador <gabriemperador@gmail.com>
2024-05-25 20:44:37 -0700

    code is not fully functioning but i want to upload

--- README.md
+++ README.md
@@ -1,27 +1,41 @@
-# WorkDayScheduler5
+# Work Day Scheduler
 
-## ##Description
+## Description
 
+A simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
 
 ## Table of Contents
--Installation
--Built With
--Usage
--Contributing
--License
--Installation
 
-## Buillt With:
-HTML
-CSS
-JavaScript
+- Installation
+- Built With
+- Usage
+- Contributing
+- License
+
+## Installation
+
+To install the Work Day Scheduler, simply download the files and open the index.html file in your browser.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- Day.js
+
+## Usage
+
+This application is designed to help users plan their workday by scheduling tasks for each hour.
 
 ## Contributing
+
 Made by Gabby
 
 ## Website
 
+[Link to the Work Day Scheduler](https://gemperador.github.io/WorkDayScheduler5/)
 
 ## Screenshot
 
-## License 
+![Screenshot of the Work Day Scheduler](image.png)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//$(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,64 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+ // });
+
+ $(document).ready(function() {
+  // Display the current day at the top of the calendar
+  const currentDay = textmoment().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(currentDay);
+
+  // Business hours from 9 AM to 5 PM
+  const businessHours = [
+    { time: '9AM', hour: 9 },
+    { time: '10AM', hour: 10 },
+    { time: '11AM', hour: 11 },
+    { time: '12PM', hour: 12 },
+    { time: '1PM', hour: 13 },
+    { time: '2PM', hour: 14 },
+    { time: '3PM', hour: 15 },
+    { time: '4PM', hour: 16 },
+    { time: '5PM', hour: 17 },
+  ];
+
+  // Generate time blocks
+  businessHours.forEach(({ time, hour }) => {
+    const currentHour = dayjs().hour();
+
+    let timeBlockClass = '';
+    if (hour < currentHour) {
+      timeBlockClass = 'past';
+    } else if (hour === currentHour) {  
+      timeBlockClass = 'present';
+    } else {
+      timeBlockClass = 'future';
+    }
+
+    const timeBlock = `
+      <div id="hour-${hour}" class="row time-block ${timeBlockClass}">
+        <div class="col-2 col-md-1 hour text-center py-3">${time}</div>
+        <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
+    `;
+
+    $('.container-fluid').append(timeBlock);
   });
+
+  // Load saved events from local storage
+  businessHours.forEach(({ hour }) => {
+    const savedEvent = localStorage.getItem(`event-${hour}`);
+    if (savedEvent) {
+      $(`#hour-${hour} .description`).val(savedEvent);
+    }
+  });
+
+  // Save event to local storage
+  $('.container-fluid').on('click', '.saveBtn', function() {
+    const hour = $(this).parent().attr('id').split('-')[1];
+    const event = $(this).siblings('.description').val();
+    localStorage.setItem(`event-${hour}`, event);
+  });
+});
